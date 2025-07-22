2ebc9040754bc8bde6dc1e74727c34f68893e08b
Gilbert <gmutai1194@gmail.com>
2025-07-22 18:27:53 +0300

    Health check view added

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path, include
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("healthz/", health_check),
     path("", include("tasks.urls")),
 ]

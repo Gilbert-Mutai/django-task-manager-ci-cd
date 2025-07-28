370052828c42fd13f66ecffe656a3d97c27e66fb
Gilbert <gmutai1194@gmail.com>
2025-07-28 18:27:43 +0300

    urls updated

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -19,8 +19,10 @@ from django.contrib import admin
 from django.http import JsonResponse
 from django.urls import path, include
 
+
 def health_check(request):
     return JsonResponse({"status": "ok"})
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),

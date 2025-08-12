50eca24e07851b59aa97170b85e4c201633f7925
Gilbert <gmutai1194@gmail.com>
2025-08-12 16:41:03 +0300

    Add allowed hosts

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -18,7 +18,12 @@ SECRET_KEY = (
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'taskmanager-alb-50046014.us-east-1.elb.amazonaws.com',
+    'localhost',
+    '127.0.0.1'
+]
+
 
 INSTALLED_APPS = [
     "django.contrib.admin",
